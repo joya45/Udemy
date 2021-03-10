81f83dfd4934813561c10e340a7092ce5d37f14d
Joya <hhhhh>
2021-03-10 10:57:31 +0530

    add string

--- Udemy/ViewController.swift
+++ Udemy/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
     var abc:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      abc = "Joya"
+        
     }
 
 
